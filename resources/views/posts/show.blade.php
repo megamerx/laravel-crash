64fb84362281ce238130fdbf700f06178aeeb4ad
Elmer Baluyo <elmer.baluyo@emersion.com>
2021-07-08 14:42:52 +0800

    add show post

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex justify-center">
+        <div class="w-4/12 bg-white p-6 rounded-lg">
+
+            <x-post :post="$post" />
+        </div>
+    </div>
+@endsection
--- resources/views/users/posts/index.blade.php
+++ resources/views/users/posts/index.blade.php
@@ -2,32 +2,35 @@
 
 @section('content')
     <div class="flex justify-center">
-        <div class="w-8/12">>
-            <div class="p-6">
-                <h1 class="text-2xl font-medium mb-1">{{ $user->name }}</h1>
-                <p>Posted {{ $posts->count() }}
-                    {{ Str::plural('post', $posts->count()) }}
-                    and received
-                    {{ $user->receivedLikes()->count() }}
-                    {{ Str::plural('like', $user->receivedLikes()->count()) }}</p>
-            </div>
+        <div class="w-9/12 bg-white p-6 rounded-lg">
+            <form action="{{ route('posts')}}" method="post">
+                @csrf
+                <div class="mb-4">
+                    <label for="body" class="sr-only">Body</label>
+                    <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 border-2 w-full p-4 rounded-lg
+                    @error('body') border-red-500 @enderror" placeholder="Post something!"></textarea>
 
-            <div class=" bg-white p-6 rounded-lg">
-                @if ($posts->count())
-                    @foreach ($posts as $post)
-                        <x-post :post="$post" />
-                    @endforeach
+                    @error('body')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
 
-                    {{ $posts->links() }}
-                @else
-                    <p> {{ $user->name}} does not have any posts </p>
-                @endif
-            </div>
+                <div>
+                    <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full" value="">Post</button>
+                </div>
+            </form>
 
-        </div>
+            @if ($posts->count())
+                @foreach ($posts as $post)
+                    <x-post :post="$post" />
+                @endforeach
 
-        <div class="">
-
+                {{ $posts->links() }}
+            @else
+                <p>There are no posts</p>
+            @endif
         </div>
     </div>
 @endsection
